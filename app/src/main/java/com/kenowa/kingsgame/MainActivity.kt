f9669ddd7d3674ae3cdbb50275cab0b646753992
Ken Owashi <ken.owashi@udea.edu.co>
2020-07-11 22:31:32 -0500

    Agregar fragmentos

--- app/src/main/java/com/kenowa/kingsgame/MainActivity.kt
+++ app/src/main/java/com/kenowa/kingsgame/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_perfil, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
